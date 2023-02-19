d029b8e2568b04587390234e29cb28f2385e7211
fodare <folorunsodamilare007@hotmail.com>
2023-02-19 21:48:34 +0100

    Sample Console logger example

---
+++ ILoggerExampleApp/ILoggerExampleApp/Models/WeatherModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ILoggerExampleApp.Models
+{
+    public class WeatherModel
+    {
+        public DateTime WeatherDate { get; set; }
+        public float temperatureC { get; set; }
+        public float temperatureF { get; set; }
+        public string summary { get; set; }
+    }
+}
