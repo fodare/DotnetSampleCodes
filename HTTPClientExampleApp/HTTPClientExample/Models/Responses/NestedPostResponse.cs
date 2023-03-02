af154da39fefdedc0558a7b3187ce2b3e10c5356
fodare <folorunsodamilare007@hotmail.com>
2023-03-02 21:09:28 +0100

    Added body field

--- HTTPClientExampleApp/HTTPClientExample/Models/Responses/NestedPostResponse.cs
+++ HTTPClientExampleApp/HTTPClientExample/Models/Responses/NestedPostResponse.cs
@@ -5,6 +5,7 @@
         public int postId { get; set; }
         public int id { get; set; }
         public string name { get; set; }
-        public string email { get; set; }   
+        public string email { get; set; }
+        public string body { get; set; }
     }
 }
