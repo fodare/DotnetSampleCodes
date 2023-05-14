fec85ab9374cfa83a95c05f55a122c686ab21bbe
fodare <folorunsodamilare007@hotmail.com>
2023-05-14 14:02:25 +0200

    Intro to RabbitMQ

---
+++ RabbitMQSample/NewTask/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+
+public class NewTask
+{
+    public static void Main(string[] args)
+    {
+        // Connection setup
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+
+        // Decleare queue and send message
+        channel.QueueDeclare(queue: "hello",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null
+        );
+
+        //const string message = "Sender says hello!";
+        for (int i = 11; i <= 21; i++)
+        {
+            var messageBody = Encoding.UTF8.GetBytes($"Producer message no {i}");
+            channel.BasicPublish(exchange: string.Empty, routingKey: "hello", basicProperties: null, body: messageBody);
+            Console.WriteLine($"Sender sent message. {i}");
+        }
+        Console.WriteLine($"\nPlease press enter to exit");
+        Console.ReadLine();
+
+    }
+}
+
+
+
---
+++ RabbitMQSample/Receive/Receive.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+// Connection setup
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// Decleare queue and receive message. Consummer might start before publisher
+// So queue is created regardless of starting node.
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+Console.WriteLine($"Consummer is waiting on messages!");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Consumer received message: {message}");
+};
+channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
