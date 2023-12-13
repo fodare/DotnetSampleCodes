8d144904ca5f8bb0351c0f395c257a6025c2350a
fodare <folorunsodamilare007@hotmail.com>
2023-12-13 19:08:50 +0100

    Made class   field public.

--- Apis/UserApi/Dtos/LoginConfirmDto.cs
+++ Apis/UserApi/Dtos/LoginConfirmDto.cs
@@ -6,7 +6,7 @@ namespace UserApi.Dtos
 {
     public class LoginConfirmDto
     {
-        byte[] PasswordHash { get; set; } = new byte[0];
-        byte[] PasswordSalt { get; set; } = new byte[0];
+        public byte[] PasswordHash { get; set; } = new byte[0];
+        public byte[] PasswordSalt { get; set; } = new byte[0];
     }
 }
